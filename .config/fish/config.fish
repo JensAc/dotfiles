e4585136954f91b07e1d2c5c6ba0dd1f916e5f19
Jens Schneider <jens.schneider.ac@gmail.com>
2021-03-11 09:18:53 +0100

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,8 @@ set -x EDITOR vim
 set -x LIBVA_DRIVER_NAME iHD 
 
 alias yuview="~/Software/YUView/YUViewApp/YUView"
-alias matlab="~/Software/Matlab2019a/bin/matlab"
-#alias rdplot="PYTHONPATH=~/Software/RDPlot/src/ python ~/Software/RDPlot/src/rdplot/__main__.py"
+#alias matlab="~/Software/Matlab2019a/bin/matlab"
+
 alias code="code --disable-gpu"
 alias gitdotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME' 
 
@@ -13,18 +13,8 @@ alias l="ls -l"
 alias mv="mv -i"
 alias rm="rm -i"
 
-#xmodmap -e "keycode 44 = j J Left Left Left dead_horn dead_hook"
-#xmodmap -e "keycode 45 = k K Down Down Down ampersand kra"
-#xmodmap -e "keycode 46 = l L Right Right Right Lstroke lstroke"
-#xmodmap -e "keycode 31 = i I Up Up Up idotless rightarrow"
-#xmodmap -e "keycode 33 = p P Prior Prior Prior"
-#xmodmap -e "keycode 47 = odiaeresis Odiaeresis Next Next  Next"
-#xmodmap -e "keycode 30 = u U Home Home Home"
-#xmodmap -e "keycode 32 = o O End End End"
-#xmodmap -e "keycode 43 = h H Delete Delete Delete"
-#xmodmap -e "keycode 66 = Mode_switch"
-
 set -x PATH /home/urbi/.local/bin $PATH
+set -x PATH /home/urbi/Software/Matlab2019a/bin/ $PATH
 
 set -x PATH /home/urbi/perl5/bin $PATH
 set -x PERL5LIB /home/urbi/perl5/lib/perl5 $PERL5LIB
